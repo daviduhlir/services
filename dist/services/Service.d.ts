fd486fbdd7ae9ffed813513fb719bd19f3d2ea8a
david.uhlir <posta@daviduhlir.cz>
2022-06-19 08:20:16 +0000

    build

--- dist/services/Service.d.ts
+++
@@ -1,9 +0,0 @@
-export declare const SERVICE_INITIALIZE_ACCESSOR: unique symbol;
-export declare class Service {
-    private internalEmitter;
-    private initDone;
-    awaited(): Promise<unknown>;
-    protected waitingDependencies: Service[];
-    protected initialize(dependecies?: Service[]): Promise<void>;
-    [SERVICE_INITIALIZE_ACCESSOR]: () => Promise<void>;
-}
--- dist/utils/errors.d.ts
+++
@@ -1,3 +0,0 @@
-export declare class ServiceNotFoundError extends Error {
-    constructor(message: string);
-}
